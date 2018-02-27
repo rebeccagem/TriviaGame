9b81dd18b2729adc543a0d59a3f64f57d66be9b0
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-02-26 19:05:15 -0600

    added end screen with counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,10 @@
 $(document).ready(function () {
-    /* You'll create a trivia game that shows only one question until the player answers it or their time runs out.
-    
-    If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
-    
-    The scenario is similar for wrong answers and time-outs.
-    
-    If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
-    If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
-    On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page). */
+
+    var unanswered = 0;
+    var correctAnswers = 0;
+    var wrongAnswers = 0;
+    var playerGuess;
+    var number;
 
     //make objects for each question
     var questionOne = {
@@ -31,24 +28,19 @@ $(document).ready(function () {
         image: "http://lorempixel.com/400/200/"
     };
 
-    var allQuestions = [questionOne, questionTwo, questionThree];
+    var intervalId;
 
-    var playerGuess;
+    var allQuestions = [questionOne, questionTwo, questionThree];
 
     var results = ["Correct", "Incorrect", "Time's Up"];
 
-    console.log(allQuestions[2]);
     var i = 0;
-    ///when player clicks an answer
-
-    var number;
-    var intervalId;
-    //move question information into DOM
 
     gameLoop();
 
+    $("#show-number").html("<h2>" + number + "</h2>");
+
     $(".answerButtons").on("click", function () {
-        console.log("HI");
         console.log(this.value);
 
         playerGuess = this.value;
@@ -58,6 +50,7 @@ $(document).ready(function () {
             $("#resultsScreen").show();
             $("#result").text(results[0]);
             $("#resultImg").attr("src", allQuestions[i].image);
+            correctAnswers++;
             stop();
             i++;
             setTimeout(gameLoop, 2000);
@@ -70,6 +63,7 @@ $(document).ready(function () {
             $("#resultsScreen").show();
             stop();
             i++;
+            wrongAnswers++;
             setTimeout(gameLoop, 2000);
 
         }
@@ -78,22 +72,19 @@ $(document).ready(function () {
     function gameLoop() {
         $("#gameScreen").show();
         $("#resultsScreen").hide();
-        number = 10;
-        if (i < allQuestions.length) {
-            populateDOM(allQuestions[i]);
-        }
-        timer();
+        populateDOM(allQuestions[i])
+        run();
     }
 
     //  When the stop button gets clicked, run the stop function.
     //  When the resume button gets clicked, execute the run function.    
-    function timer() {
-        $("#stop").on("click", stop);
-        $("#resume").on("click", run);
-        run();
-    }
+
+    $("#answers").on("click", stop);
+
+    $("#resume").on("click", run);
 
     function run() {
+        number = 10;
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
     }
@@ -107,13 +98,13 @@ $(document).ready(function () {
         if (number === 0) {
             //  ...run the stop function.
             //  Alert the user that time is up.
-            alert("Time Up!");
             $("#result").text(results[2]);
             $("#resultImg").attr("src", allQuestions[i].image);
             $("#gameScreen").hide();
             $("#resultsScreen").show();
             stop();
             i++;
+            unanswered++;
             setTimeout(gameLoop, 2000);
         }
 
@@ -127,20 +118,30 @@ $(document).ready(function () {
 
     //register click for correct answer
     function populateDOM(currentQuestion) {
-        $("#question").text(currentQuestion.question);
-        $("#A").text(currentQuestion.answers[0]);
-        $("#B").text(currentQuestion.answers[1]);
-        $("#C").text(currentQuestion.answers[2]);
-        $("#D").text(currentQuestion.answers[3]);
-        $("#A").attr("value", currentQuestion.answers[0]);
-        $("#B").attr("value", currentQuestion.answers[1]);
-        $("#C").attr("value", currentQuestion.answers[2]);
-        $("#D").attr("value", currentQuestion.answers[3]);
+        if (allQuestions.length >= (i + 1)) {
+            console.log("I: " + i + " All Q: " + allQuestions.length);
+            $("#question").text(currentQuestion.question);
+            $("#A").text(currentQuestion.answers[0]);
+            $("#B").text(currentQuestion.answers[1]);
+            $("#C").text(currentQuestion.answers[2]);
+            $("#D").text(currentQuestion.answers[3]);
+            $("#A").attr("value", currentQuestion.answers[0]);
+            $("#B").attr("value", currentQuestion.answers[1]);
+            $("#C").attr("value", currentQuestion.answers[2]);
+            $("#D").attr("value", currentQuestion.answers[3]);
+        }
+        else writeStats();
     }
 
 
-});
+    function writeStats() {
+        $("#gameScreen").hide();
+        $("#correctAnswers").text("Correct Answers: " + correctAnswers);
+        $("#wrongAnswers").text("Wrong Answers: " + wrongAnswers);
+        $("#unanswered").text("Unanswered: " + unanswered);
+    }
 
+});
 
 
     //  Interval Demonstration
